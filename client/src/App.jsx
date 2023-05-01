77facc9373121271d03916205810e7dca04ae83a
Mario Ponce <mponpol@gmail.com>
2023-05-01 11:03:25 +0200

    Add Pages and react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,30 @@
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Single from './pages/Single';
+import Write from './pages/Write';
 import './App.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Home</div>,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
-    <>
-      <div>Hola</div>
-    </>
+    <div className="app">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
