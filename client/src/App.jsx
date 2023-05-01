f152c6666297223b2fa78eb78d3417fa872902c2
Mario Ponce <mponpol@gmail.com>
2023-05-01 12:28:21 +0200

    Add Sass, define routes and Layout, add Login and Register content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,47 @@
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Home from './pages/Home';
 import Single from './pages/Single';
 import Write from './pages/Write';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 import './App.css';
+import './style.scss';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Home</div>,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/post/:id',
+        element: <Single />,
+      },
+      {
+        path: '/write',
+        element: <Write />,
+      },
+    ],
   },
   {
     path: '/register',
@@ -23,7 +56,9 @@ const router = createBrowserRouter([
 function App() {
   return (
     <div className="app">
-      <RouterProvider router={router} />
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
