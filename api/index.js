8235e8a3e25c4e60e53ebda13c9fda100ea7fadd
Mario Ponce <mponpol@gmail.com>
2023-05-05 12:32:47 +0200

    Change to mysql2, set proxy, finish Register route

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import postRoutes from './routes/posts.js';
 const app = express();
 
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,59 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Register() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = evt => {
+    setInputs(prev => ({ ...prev, [evt.target.name]: evt.target.value }));
+  };
+
+  const handleSubmit = async evt => {
+    evt.preventDefault();
+    try {
+      await axios.post('api/auth/register', inputs);
+      navigate('/login');
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
   return (
     <div className="auth">
       <h1>Register</h1>
       <form>
-        <input required type="text" placeholder="username" />
-        <input required type="email" placeholder="email" />
-        <input required type="password" placeholder="password" />
-        <button>Register</button>
-        <p>This is an error!</p>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Register</button>
+        {err && <p>{err}</p>}
         <span>
           Do you have an account? <Link to="/login">Login</Link>
         </span>
